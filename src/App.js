d1d2c5c9469be6bfe96f8df19262b73361fbb321
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-27 11:05:00 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { Fragment } from "react";
-import AddUsers from './components/Users/AddUsers'
-const App=()=> {
-  return (
-    <Fragment>
-      <AddUsers onAddUser={addUserHnadler} />
-      <UserList users={userLists} />
-    </Fragment>
-  );
+import React,{useState} from 'react';
+import Login from './components/Login';
+
+import './App.css';
+const App=()=>{
+        const [userList, setUserList]=useState([]);
+        const addNewUserHandler=(uName,uAge,uCollage)=>{
+                setUserList((prevUserList)=>{
+                        return[...prevUserList,{name:uName,age:uAge,collage:uCollage}]
+                })
+        }
+
+
+
+
+        return(
+           <div>
+            <Form onAddUser={addNewUserHandler}/>
+            <UserList users={userList} />
+
+            <Login  />
+           </div> 
+
+        );
 }
 
 export default App;
